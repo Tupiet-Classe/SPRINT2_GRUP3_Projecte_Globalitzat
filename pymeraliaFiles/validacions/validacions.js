c84aa069329f04ea0b213f631b5fd37d55eadc31
Julen Martinez <julenmartinez@iesmontsia.org>
2022-10-26 19:44:39 +0200

    validacions

--- pymeraliaFiles/validacions/validacions.js
+++ pymeraliaFiles/validacions/validacions.js
@@ -15,3 +15,13 @@ export function email(string) {
   let regex = /^\w+@\w+\.\w+$/
   return regex.test(string.trim())
 }
+
+export function caracters(valor){
+var iChars = "<>";
+
+for (var i = 0; i < valor; i++) {
+    if (iChars.indexOf(valor.charAt(i)) != -1) {
+        return false;
+    }
+}
+}
