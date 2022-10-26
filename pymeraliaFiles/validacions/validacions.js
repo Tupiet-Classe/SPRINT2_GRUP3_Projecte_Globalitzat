5f297539f9527df03e7a22082bc312f5e3fa9a88
MSancho97 <marcsancho@iesmontsia.org>
2022-10-26 19:51:42 +0200

    comentaris

--- pymeraliaFiles/validacions/validacions.js
+++ pymeraliaFiles/validacions/validacions.js
@@ -1,9 +1,11 @@
+//Validació per a que el camp no estigui buit
 export function notEmpty(valor) {
   if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
     return false;
   }
 }
 
+//Validació per a llargaria màxima
 export function length(string, minLength, maxLength) {
   if (maxLength)
       return string.length >= minLength && string.length <= maxLength
@@ -11,11 +13,13 @@ export function length(string, minLength, maxLength) {
       return string.length >= minLength
 }
 
+//Validació per a que les dades introduides siguin emails
 export function email(string) {
   let regex = /^\w+@\w+\.\w+$/
   return regex.test(string.trim())
 }
 
+//Validació per a caracters especials "<>"
 export function caracters(valor){
 var iChars = "<>";
 
