05042ab6a69a7a5c8a080c5e764cba436dfb7e5e
Julen Martinez <julenmartinez@iesmontsia.org>
2022-10-26 20:15:18 +0200

    fora exports

--- pymeraliaFiles/validacions/validacions.js
+++ pymeraliaFiles/validacions/validacions.js
@@ -1,22 +1,22 @@
-export function notEmpty(valor) {
+function notEmpty(valor) {
   if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
     return false;
   }
 }
 
-export function length(string, minLength, maxLength) {
+function length(string, minLength, maxLength) {
   if (maxLength)
       return string.length >= minLength && string.length <= maxLength
   else
       return string.length >= minLength
 }
 
-export function email(string) {
+function email(string) {
   let regex = /^\w+@\w+\.\w+$/
   return regex.test(string.trim())
 }
 
-export function caracters(valor){
+function caracters(valor){
 var iChars = "<>";
 
 for (var i = 0; i < valor; i++) {
