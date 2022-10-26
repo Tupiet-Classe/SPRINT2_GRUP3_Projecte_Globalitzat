261174b2e05495e34fe014f4fca6c475476659e8
Julen Martinez <julenmartinez@iesmontsia.org>
2022-10-26 20:18:46 +0200

    exports fora

--- pymeraliaFiles/validacions/validacions.js
+++ pymeraliaFiles/validacions/validacions.js
@@ -1,27 +1,27 @@
-export function notEmpty(valor) {
-  if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-    return false;
-  }
-}
-
-export function length(string, minLength, maxLength) {
-  if (maxLength)
-      return string.length >= minLength && string.length <= maxLength
-  else
-      return string.length >= minLength
-}
-
-export function email(string) {
-  let regex = /^\w+@\w+\.\w+$/
-  return regex.test(string.trim())
-}
-
-export function caracters(valor){
-var iChars = "<>";
-
-for (var i = 0; i < valor; i++) {
-    if (iChars.indexOf(valor.charAt(i)) != -1) {
-        return false;
+function notEmpty(valor) {
+    if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+      return false;
     }
-}
-}
+  }
+  
+  function length(string, minLength, maxLength) {
+    if (maxLength)
+        return string.length >= minLength && string.length <= maxLength
+    else
+        return string.length >= minLength
+  }
+  
+  function email(string) {
+    let regex = /^\w+@\w+\.\w+$/
+    return regex.test(string.trim())
+  }
+  
+  function caracters(valor){
+  var iChars = "<>";
+  
+  for (var i = 0; i < valor; i++) {
+      if (iChars.indexOf(valor.charAt(i)) != -1) {
+          return false;
+      }
+  }
+  }
