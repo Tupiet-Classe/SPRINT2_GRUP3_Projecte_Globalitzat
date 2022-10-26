07c17ec29c39419d1c31fcb887782f8ae6e7549a
Tupiet <aleixescrihuela@gmail.com>
2022-10-26 20:20:38 +0200

    Petit canvi d'estil

--- pymeraliaFiles/validacions/validacions.js
+++ pymeraliaFiles/validacions/validacions.js
@@ -1,27 +1,25 @@
 function notEmpty(valor) {
-    if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-      return false;
+  if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+    return false
+  }
+}
+
+function length(string, minLength, maxLength) {
+  if (maxLength) return string.length >= minLength && string.length <= maxLength
+  else return string.length >= minLength
+}
+
+function email(string) {
+  let regex = /^\w+@\w+\.\w+$/
+  return regex.test(string.trim())
+}
+
+function caracters(valor) {
+  var iChars = '<>'
+
+  for (var i = 0; i < valor; i++) {
+    if (iChars.indexOf(valor.charAt(i)) != -1) {
+      return false
     }
   }
-  
-  function length(string, minLength, maxLength) {
-    if (maxLength)
-        return string.length >= minLength && string.length <= maxLength
-    else
-        return string.length >= minLength
-  }
-  
-  function email(string) {
-    let regex = /^\w+@\w+\.\w+$/
-    return regex.test(string.trim())
-  }
-  
-  function caracters(valor){
-  var iChars = "<>";
-  
-  for (var i = 0; i < valor; i++) {
-      if (iChars.indexOf(valor.charAt(i)) != -1) {
-          return false;
-      }
-  }
-  }
+}
